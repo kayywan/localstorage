6d1fb9c7466392ba076d060f8254eaa134a5e751
KayyWan <k.wan9311@gmail.com>
2020-07-04 16:48:43 -0400

    uploading day planner

---
+++ planner.js
@@ -0,0 +1,13 @@
+var saveButton = document.querySelector("saveBtn");
+var textbox = document.querySelector("textbox");
+
+const saveToLocal 
+    $(saveButton).on('click', function() {
+        
+        textbox.textContent
+        localStorage.setItem("textbox", JSON.stringify(textbox));
+        localStorage.getItem ("textbox");
+        JSON.parse(localStorage.getItem);
+
+        
+    })
